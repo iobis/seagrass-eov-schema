0ce9e56d37478fb441d4be89217b18e70a14df23
Pieter Provoost <pieterprovoost@gmail.com>
2025-02-02 11:58:24 +0100

    cleanup

--- test.py
+++
@@ -1,8 +0,0 @@
-from frictionless import validate
-
-report = validate("datapackage.json")
-
-if report.valid:
-    print("The Data Package is valid!")
-else:
-    print("The Data Package has errors:", report.flatten(["code", "message"]))
